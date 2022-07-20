13c6f0e390bcc112c14ec0a059dd4b7689e80666
lahcenegue <98424839+lahcenegue@users.noreply.github.com>
2022-07-20 08:16:23 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,15 +1,23 @@
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID "TMPLWkdIfCY3"
+#define BLYNK_DEVICE_NAME "nodemcu"
+#define BLYNK_AUTH_TOKEN "RtVHnQjxsi1B5fdZDFti3JihcAYcKyGP"
 
-#define BLYNK_TEMPLATE_ID           "TMPLpTMTeHqd"
-#define BLYNK_DEVICE_NAME           "Courant"
-#define BLYNK_AUTH_TOKEN            "DQrsk21VXPVuM99w0NurSi9c-4ASXbO3"
+// ESP8266 Analog Pin ADC0 = A0 
+#define analogPin A0                
 
 #define BLYNK_PRINT Serial
 
+
+// Include Emon Library
 #include <EmonLib.h> 
+
+
 #include <Wire.h> 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
@@ -19,26 +27,33 @@ char ssid[] = "Idoom 4G_BDF13";
 char pass[] = "URD2021@at";
 
 
-#define ADC_INPUT 34
+#define ADC_INPUT 0                 /* Variable to store Output of ADC */
 
-
+// Create an instance
 EnergyMonitor emon1;
+
+
 BlynkTimer timer;
 
 
 void setup() {
-  Serial.begin(115200); 
-  Blynk.begin(auth, ssid, pass);  
-  emon1.current(ADC_INPUT, 6.66); 
+  Serial.begin(115200);                      /* Initialize serial communication at 115200 */
+  Blynk.begin(auth, ssid, pass);
+
+  //calibration 
+  emon1.current(ADC_INPUT, 39);           
   timer.setInterval(250L, AnalogPinRead);
 }
 
 
 void AnalogPinRead() {
+
+  //calculate irms (5588 fixed valeur)
   double Irms = emon1.calcIrms(5588); 
+  
   Serial.print("sensor = ");  
   Serial.println(Irms);  
-  Blynk.virtualWrite(V1, Irms);  
+  Blynk.virtualWrite(V2, Irms);  
 }
 
 
